2f04c473ebdaaf7505e2eb130b13de29410379d0
Elvio Toccalino <etoccalino@fi.uba.ar>
2012-06-26 00:41:06 -0300

    category aspects.

--- main/interface.py
+++ main/interface.py
@@ -69,3 +69,60 @@ def inclusive_search(qdict):
     else:
         queryset = Imagenes.objects.none()
     return queryset
+
+
+###############################################################################
+
+
+class CategoryAspect(Aspect):
+    """Category plugins are common. This class isolates common behaviuor.
+
+    The 'category_name', holding the categories of interest to the user,
+    is expected in the qdict.
+
+    An extending plugin should provide:
+    - the PluginModel which holds the data for the plugin.
+    - the name of the field in the plugin model which is relevant.
+    - the 'category_name', the name expected in the qdict, which identifies
+      this category to the end user.
+    - the 'categories', with all possible values of the category.
+    - the callable 'categorize_image', which takes an Imagenes and returns
+      the particular category it belongs to (only one, in this implementation).
+    """
+
+    def __init__(self, plugin_model, category_field_name, category_name,
+                 categories, *args, **kwargs):
+        super(CategoryAspect, self).__init__(*args, **kwargs)
+        self.plugin_model = plugin_model
+        self.category_field_name = category_field_name
+        self.category_name = category_name
+        self.categories = categories
+
+    def process_image(self, image_instance):
+        """Store the resulting category of the image in the weight table."""
+        categorized = self.plugin_model(image=image_instance)
+        category = self.categorize_image(image_instance)
+        setattr(categorized, self.category_field_name, category)
+        categorized.save()
+
+    def filter_images(self, qdict, queryset):
+        """Apply this category filter to the Imagenes queryset.
+
+        Assuming that the presence of 'weight' key in the qdict indicates its
+        value is a non-empty collection, then... TODO """
+        try:
+            # assume the popped 'categories' its a non-emtpy collection.
+            categories = qdict.pop(self.category_name)
+
+            # create a list of filters based on the category criteria.
+            queries = []
+            query_selector = 'image__' + self.category_field_name
+            for category in filter(lambda s: s in self.categories, categories):
+                queries += queryset.filter(**{query_selector: category})
+
+            # the resulting queryset is the OR'ed of the filtered.
+            queryset = reduce(lambda q1, q2: q1 | q2, queries)
+        except KeyError:
+            # no criteria... this plugin does nothing.
+            pass
+        return queryset
--- main/plugins/weight.py
+++ main/plugins/weight.py
@@ -1,6 +1,7 @@
 from django.db import models
 from models import PluginModel
-from interface import Aspect
+from interface import CategoryAspect
+
 
 _weights = (('t', 'tiny'), ('s', 'small'), ('m', 'medium'), ('l', 'large'))
 
@@ -9,41 +10,22 @@ class ImageWeight(PluginModel):
     weight = models.CharField(max_length=1, choices=_weights)
 
 
-class Weight(Aspect):
-    # provide the acceptable key-value combinations for the GET request.
-    acceptable_qstring_values = {'weight': [s[1] for s in _weights]}
-
+class Weight(CategoryAspect):
+    configuration = {
+        plugin_model: ImageWeight,
+        category_name: 'weight',
+        category_field_name: 'weight',
+        categories: ['tiny', 'small', 'medium', 'large']
+        }
+    
     def __init__(self, lower_bound=100, medium_bound=1024, upper_bound=3072):
         """Set the bounds used during processing, to categorize the images
         (given in kilobytes)."""
+        super(Weight, self).__init__(**configuration)
         self.lower_bound = lower_bound
         self.medium_bound = medium_bound
         self.upper_bound = medium_bound
 
-    def process_image(self, image_instance):  # DEBUG
-        """Store the weight results of the image in the weight table."""
-        weight_register = ImageWeight(image=image_instance)
-        weight_register.weight = 'small'  # DEBUG
-        weight_register.save()
-
-    def filter_images(self, qdict, queryset):
-        """Apply the weight filter to the Imagenes queryset.
-
-        Assuming that the presence of 'weight' key in the qdict indicates its
-        value is a non-empty collection, then... TODO """
-        try:
-            # assume the popped 'weights' its a non-emtpy collection.
-            weights = qdict.pop('weight')
-
-            # create a list of filters based on the weight's criteria.
-            acceptable = Weight.acceptable_qstring_values
-            queries = []
-            for weight in filter(lambda s: s in acceptable['weight'], weights):
-                queries += queryset.filter(image__weight=weight)
-
-            # the resulting queryset is the OR'ed of the filtered.
-            queryset = reduce(lambda q1, q2: q1 | q2, queries)
-        except KeyError:
-            # no 'weight' criteria... this plugin does nothing.
-            pass
-        return queryset
+    def categorize_image(self, image_instance):  # DEBUG
+        """Return the weight category this image belongs to."""
+        return "small"
